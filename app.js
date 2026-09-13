e7c0f08c936fcf69d0bc9de7a6212123dd092050
Anupam <vishwakarmaanupam01@gmail.com>
2026-09-13 21:52:08 +0530

    updated massage

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const server = http.createServer((req, res) => {
     });
 
     res.end(`
-        <h1>hiii kaka bhula chok  ${APP_ENV} ITCAMPUSGURU </h1>
+        <h1>bhola me man dola   ${APP_ENV} ITCAMPUSGURU </h1>
         <p>Application deployed using GitHub Actions → AWS EC2</p>
         <p>Environment: <strong>${APP_ENV}</strong></p>
     `);
